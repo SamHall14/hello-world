ad6a43f6e214efc11a7a2bed3d92fef23ba1c871
SamHall14 <63981450+SamHall14@users.noreply.github.com>
2021-02-01 03:20:38 -0500

    Update bonjour.c

--- bonjour.c
+++ bonjour.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
- printf("Hello, there were %d arguments entered!", argc);
+ printf("Hello, there were %d arguments entered!\nPeePeePooPoo", argc);
  return 0;
 }
